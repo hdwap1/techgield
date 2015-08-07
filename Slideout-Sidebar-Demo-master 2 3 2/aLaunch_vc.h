c8dddfe8928609348434aeba438eb0874b8afcb1
hdwap1 <indianic@ind676-2.local>
2015-08-07 15:50:52 -0700

    all

---
+++ Slideout-Sidebar-Demo-master 2 3 2/aLaunch_vc.h
@@ -0,0 +1,14 @@
+//
+//  aLaunch_vc.h
+//  MyTravelExperienceApp
+//
+//  Created by patel Rahul on 24/07/15.
+//  Copyright (c) 2015 MTE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MainViewController.h"
+@interface aLaunch_vc : UIViewController
+- (IBAction)aSkipbtnAct:(UIButton *)sender;
+
+@end
